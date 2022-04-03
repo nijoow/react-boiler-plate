dcacf1b31e4a29d9fc6385d56f99fa9d2985a150
nijoow <nijoow1127@gmail.com>
2022-04-03 15:10:35 +0900

    백엔드 회원가입, 로그인, 로그아웃 기능

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -7,7 +10,7 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     trim: true,
-    unique: 1,
+    unique: true,
   },
   password: {
     type: String,
@@ -20,6 +23,48 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  let user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        // Store hash in your password DB.
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
+userSchema.methods.generateToken = function (callback) {
+  let user = this;
+  let token = jwt.sign(user._id.toHexString(), "secretToken");
+  user.token = token;
+  user.save((err, user) => {
+    if (err) return callback(err);
+    callback(null, user);
+  });
+};
+userSchema.statics.findByToken = function (token, callback) {
+  let user = this;
+  jwt.verify(token, "secretToken", function (err, decoded) {
+    user.findOne({ _id: decoded, token: token }, (err, user) => {
+      if (err) return callback(err);
+      callback(null, user);
+    });
+  });
+};
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
